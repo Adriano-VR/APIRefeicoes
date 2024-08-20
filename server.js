9c840ea3acc93aaa8d955f61f154ad9cf4715600
adr <rodrigues.adriano2607@gmail.com>
2024-08-20 14:40:03 -0300

    historico

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const fs = require('fs');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');  // Importa a função para gerar UUIDs
+const { v4: uuidv4 } = require('uuid');
 const server = express();
 
 server.use(cors());
-server.use(express.json()); 
+server.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join(__dirname, 'data.json');
@@ -15,8 +15,33 @@ server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
-// Carregando o arquivo correto
+// Carregando o arquivo de dados
 let data = require(DATA_FILE);
+
+// Função para salvar os dados no arquivo
+function salvarDados(dados) {
+    fs.writeFileSync(DATA_FILE, JSON.stringify(dados, null, 2));
+}
+
+// Função para salvar o histórico com nome e descrição do item apenas para ações de "add"
+function salvarHistorico(itemId) {
+    let historicoItem = {
+        id: uuidv4(),
+        itemId,
+    };
+
+  
+        const item = data.items.find(item => item.id === itemId);
+        historicoItem.name = item ? item.name : 'Desconhecido';
+        historicoItem.descricao = item ? item.descricao : 'Desconhecido';
+        historicoItem.data = item ? item.data : 'Desconhecido';
+        historicoItem.users = item ? item.users : 0
+        
+    
+
+    data.historico.push(historicoItem);
+    salvarDados(data);
+}
 
 // Rota GET para buscar os itens
 server.get('/', (req, res) => {
@@ -27,7 +52,7 @@ server.get('/', (req, res) => {
     const startIndex = (page - 1) * itemsPerPage;
     const endIndex = page * itemsPerPage;
 
-    const filteredData = data.filter(item => 
+    const filteredData = data.items.filter(item =>
         item.name.toLowerCase().includes(search.toLowerCase())
     );
 
@@ -64,10 +89,13 @@ server.post('/add', (req, res) => {
         novaRefeicao.data = currentDate;
 
         // Adiciona o novo item ao array de dados
-        data.push(novaRefeicao);
+        data.items.push(novaRefeicao);
 
         // Salva os dados no arquivo
         salvarDados(data);
+
+        // Salva no histórico
+        salvarHistorico(novaRefeicao.id);
 
         // Retorna resposta de sucesso
         res.status(201).json({ message: "Item added successfully", newItem: novaRefeicao });
@@ -83,7 +111,13 @@ server.delete('/delete/:id', (req, res) => {
         const { id } = req.params;
 
         // Remove o item com o ID especificado
-        data = data.filter(item => item.id !== id);
+        const itemIndex = data.items.findIndex(item => item.id === id);
+        if (itemIndex === -1) {
+            return res.status(404).json({ message: "Item not found" });
+        }
+
+       
+        data.items.splice(itemIndex, 1);
 
         // Salva os dados atualizados no arquivo
         salvarDados(data);
@@ -96,20 +130,21 @@ server.delete('/delete/:id', (req, res) => {
     }
 });
 
-// Rota PATCH para incrementar o número de usuários
 server.patch('/increment/:id', (req, res) => {
     try {
         const { id } = req.params;
 
         // Atualiza o número de usuários para o item com o ID especificado
-        data = data.map(item =>
-            item.id === id ? { ...item, users: item.users + 1 } : item
-        );
+        const item = data.items.find(item => item.id === id);
+        if (!item) {
+            return res.status(404).json({ message: "Item not found" });
+        }
+
+        item.users += 1;
 
         // Salva os dados atualizados no arquivo
         salvarDados(data);
 
-        // Retorna resposta de sucesso
         res.status(200).json({ message: "User count incremented successfully" });
     } catch (error) {
         console.error("Erro ao incrementar usuários: ", error);
@@ -123,13 +158,17 @@ server.patch('/decrement/:id', (req, res) => {
         const { id } = req.params;
 
         // Atualiza o número de usuários para o item com o ID especificado
-        data = data.map(item =>
-            item.id === id ? { ...item, users: Math.max(item.users - 1, 0) } : item
-        );
+        const item = data.items.find(item => item.id === id);
+        if (!item) {
+            return res.status(404).json({ message: "Item not found" });
+        }
+
+        item.users = Math.max(item.users - 1, 0);
 
         // Salva os dados atualizados no arquivo
         salvarDados(data);
 
+      
         // Retorna resposta de sucesso
         res.status(200).json({ message: "User count decremented successfully" });
     } catch (error) {
@@ -138,6 +177,11 @@ server.patch('/decrement/:id', (req, res) => {
     }
 });
 
-function salvarDados(dados) {
-    fs.writeFileSync(DATA_FILE, JSON.stringify(dados, null, 2));
-}
+// Rota GET para buscar o histórico
+server.get('/historico', (req, res) => {
+    res.json({
+        items: data.historico
+    });
+});
+
+
